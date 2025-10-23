bd3d4a241171280494cf4c7bc321f8df8285b8a4
Alejandro Diez Bermejo <adbermejo@ubu.es>
2025-10-23 11:22:35 +0200

    IoT Simulator 1.0.3

--- src/app/models/position.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Representa una posición en un plano bidimensional.
- */
-export interface Position {
-    /** Coordenada en el eje X */
-    x: number;
-    /** Coordenada en el eje Y */
-    y: number;
-}
